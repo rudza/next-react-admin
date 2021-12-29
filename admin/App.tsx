bd5e21b3a7057d0940fa50588f1a2e3bfbfa9b5b
Rudolfs J <rudolfs.janitis@gmail.com>
2021-12-29 21:23:03 +0200

    chore: cleanup

--- admin/App.tsx
+++ admin/App.tsx
@@ -1,27 +1,16 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { createHashHistory } from 'history';
-import { Provider } from 'react-redux';
 
 import CustomLayout from "../components/CustomLayout";
-import { createAdminStore } from "./createAdminStore";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const history = createHashHistory();
 
 const App = () => (
-  <Provider
-  store={createAdminStore({
-      dataProvider,
-      history,
-  })}
->
-  <Admin dataProvider={dataProvider} layout={CustomLayout} history={history}>
+  <Admin dataProvider={dataProvider} layout={CustomLayout}>
     <Resource name="users" list={ListGuesser} />
     <Resource name="posts" list={ListGuesser} />
   </Admin>
-</Provider>
 );
 
 export default App;
